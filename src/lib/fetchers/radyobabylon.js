b39bd6da0ae9ad354940c3cbea245b395d40215e
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-06-23 12:27:07 +0300

    fix(fetcher): Fix radyobabylon

--- src/lib/fetchers/radyobabylon.js
+++ src/lib/fetchers/radyobabylon.js
@@ -18,22 +18,25 @@ class RadyoBabylonFetcher {
      * the first parameter and the data is second.
      */
     static fetch(callback) {
-        exec(' curl http://babylon.com.tr/tr/', (err, stdout, stderr) => {
+        exec(' curl http://api.pozitifmuzik.com/channel/main/nowplaying/', (err, stdout, stderr) => {
             if (err) {
-                debug('Cannot curl babylon.com.tr', err);
+                debug('Cannot curl api.pozitifmuzik.com', err);
                 return callback(err);
             }
 
-            let $ = cheerio.load(stdout);
-            let artist = $('.track-name .marquee-content').text() || '';
-            let title = $('.artist-name .marquee-content').text() || '';
+            try {
+                const result = JSON.parse(stdout);
+                const artist = result.data.current && result.data.current.artist && result.data.current.artist.name;
+                const title = result.data.current && result.data.current.track && result.data.current.track.name;
 
-            if (!artist || !title) {
-                debug('Not fount artist or title', $('#mrp-radyo-babylon').text());
-                return callback('Not fount artist or title');
+                if (!artist || !title) {
+                    return callback('Not fount artist or title');
+                }
+
+                callback(null, { artist, title });
+            } catch (err) {
+                callback('Could not parse json or worse:' + err);
             }
-
-            callback(null, { artist, title });
         });
     }
 }
